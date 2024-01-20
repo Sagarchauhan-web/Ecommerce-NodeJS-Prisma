cc804f678e9e707a713b40b3fb7f4233f4747bae
Sagar <Sagarchawan4646@gmail.com>
2024-01-20 17:16:57 +0530

    products crud operations

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { prismaClient } from '..';
+import { NotFoundException } from '../exceptions/notFound';
+import { ErrorCodes } from '../exceptions/root';
 
 export const createProduct = async (req: Request, res: Response) => {
   const product = await prismaClient.product.create({
@@ -10,4 +12,66 @@ export const createProduct = async (req: Request, res: Response) => {
   });
 
   return res.json(product);
+};
+
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const product = req.body;
+    if (product) product.tags = product.tags.join(',');
+
+    const result = await prismaClient.product.update({
+      where: { id: Number(req.params.id) },
+      data: product,
+    });
+
+    res.json(result);
+  } catch (error) {
+    throw new NotFoundException(
+      'Product Not Found',
+      ErrorCodes.PRODUCT_NOT_FOUND,
+    );
+  }
+};
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    await prismaClient.product.delete({
+      where: { id: Number(req.params.id) },
+    });
+
+    res.sendStatus(200);
+  } catch (error) {
+    throw new NotFoundException(
+      'Product Not Found',
+      ErrorCodes.PRODUCT_NOT_FOUND,
+    );
+  }
+};
+
+// Pagination {count: 100, data: []}
+export const listProducts = async (req: Request, res: Response) => {
+  const count = await prismaClient.product.count();
+
+  const skip = req.query.skip ? +req.query.skip : 0;
+  const products = await prismaClient.product.findMany({
+    skip: skip,
+    take: 5,
+  });
+
+  res.json({ count, products });
+};
+
+export const getProductById = async (req: Request, res: Response) => {
+  try {
+    const product = await prismaClient.product.findUnique({
+      where: { id: Number(req.params.id) },
+    });
+
+    res.json(product);
+  } catch (error) {
+    throw new NotFoundException(
+      'Product Not Found',
+      ErrorCodes.PRODUCT_NOT_FOUND,
+    );
+  }
 };
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express';
-import { createProduct } from '../controllers/product';
+import {
+  createProduct,
+  deleteProduct,
+  getProductById,
+  listProducts,
+  updateProduct,
+} from '../controllers/product';
 import { errorHandler } from '../error-handler';
 import authMiddleware from '../middlewares/auth';
 import adminMiddleware from '../middlewares/admin';
@@ -11,6 +17,30 @@ productRoutes.post(
   authMiddleware,
   adminMiddleware,
   errorHandler(createProduct),
+);
+productRoutes.put(
+  '/:id',
+  authMiddleware,
+  adminMiddleware,
+  errorHandler(updateProduct),
+);
+productRoutes.delete(
+  '/:id',
+  authMiddleware,
+  adminMiddleware,
+  errorHandler(deleteProduct),
+);
+productRoutes.get(
+  '/',
+  authMiddleware,
+  adminMiddleware,
+  errorHandler(listProducts),
+);
+productRoutes.get(
+  '/:id',
+  authMiddleware,
+  adminMiddleware,
+  errorHandler(getProductById),
 );
 
 export default productRoutes;
